9636b4e12942436f69c45023c552e56a5602b59d
Sinisha Pecov <p.sinisha12@gmail.com>
2020-10-19 20:32:11 +0200

    List ingredients fixture

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second ...')
+                self.stdout.write('Database unavailable, waiting 5 seconds...')
                 time.sleep(5)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
