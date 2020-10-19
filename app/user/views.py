f9e952cf9ecde2d7b9e46acaee79856011bee417
Sinisha Pecov <p.sinisha12@gmail.com>
2020-10-19 19:55:03 +0200

    added working tag list

--- app/user/views.py
+++ app/user/views.py
@@ -19,8 +19,8 @@ class CreateTokenView(ObtainAuthToken):
 class ManageUserView(generics.RetrieveAPIView):
     """Manage the authenticated user"""
     serializer_class = UserSerializer
-    authentication_classes = [authentication.TokenAuthentication, ]
-    permission_classes = [permissions.IsAuthenticated, ]
+    authentication_classes = (authentication.TokenAuthentication, )
+    permission_classes = (permissions.IsAuthenticated, )
 
     def get_object(self):
         """Retrieve and return authenticated user"""
